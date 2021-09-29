98cf6e9d7acfc3f570ac11acb501ac07457a0707
Ben Hunter <learntocode@benhunter.me>
2021-09-29 10:49:41 -0500

    Initial project from Spring Initializer.

---
+++ src/main/java/com/benhunterlearn/springcrudreredux/SpringCrudReReduxApplication.java
@@ -0,0 +1,13 @@
+package com.benhunterlearn.springcrudreredux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrudReReduxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCrudReReduxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/benhunterlearn/springcrudreredux/SpringCrudReReduxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benhunterlearn.springcrudreredux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrudReReduxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
